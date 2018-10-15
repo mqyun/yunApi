fbfe673e28ed01ce0c547e3845137ab469fb8ede
mqyun <17855831290@163.com>
2018-10-15 11:19:47 +0800

    接口本地调试方案

--- public/js/index.js
+++ public/js/index.js
@@ -8,13 +8,42 @@ $(document).on('click', '.form-check-item', function () {
 })
 
 $(document).on('click', '.peo-data-item.add', function () {
-  $('#data-item-box').append('<div class="data-item">\
-  <input class="data-item-input" type="text" placeholder="key">\
+  $('#data-item-box').append('<div class="data-item req-data-item">\
+  <input class="data-item-input req-data-key" type="text" placeholder="key">\
   <span>:</span>\
-  <input class="data-item-input" type="text" placeholder="value">\
+  <input class="data-item-input req-data-val" type="text" placeholder="value">\
   <div class="peo-data-item del">-</div>');
 })
 
 $(document).on('click', '.peo-data-item.del', function () {
   $(this).parents('.data-item').remove();
 })
+
+$(document).on('click', '.btn-request', function () {
+  var url = $('#urlInput').val();
+  var type = $('.form-check-item.active').data('type');
+  var data = {};
+  $('.req-data-item').each(function () {
+    var _this = $(this);
+    var dataKey = _this.find('.req-data-key').val();
+    var dataVal = _this.find('.req-data-val').val();
+    data[dataKey] = dataVal;
+  })
+  ajaxReq(url, type, data);
+})
+
+function ajaxReq (url, type, data) {
+  if (type == 'POST') {
+    data = JSON.stringify(data);
+  }
+  $.ajax({
+    url: url,
+    type: type,
+    contentType: 'application/json; charset=utf-8',
+    dataType: 'json',
+    data: data,
+    success: function (res) {
+      $('.res-con').html(JSON.stringify(res));
+    }
+  })
+}
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const router = require('koa-router')()
 
-router.prefix('/users')
+router.prefix('/user')
 
 router.get('/', function (ctx, next) {
   ctx.body = 'this is a users response!'
@@ -8,6 +8,31 @@ router.get('/', function (ctx, next) {
 
 router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response'
+})
+
+router.all('/list', async (ctx, next) => {
+  ctx.body = {
+    'code': 0,
+    data: {
+      'pageNo': 1,
+      'pageSize': 3,
+      'totalCount': 1024,
+      'list': [{
+        'id': 0,
+        'account': 'mqyun',
+        'username': 'mqy'
+      }, {
+        'id': 1,
+        'account': 'mqyun1',
+        'username': 'mqy1'
+      }, {
+        'id': 2,
+        'account': 'mqyun2',
+        'username': 'mqy2'
+      }],
+      'message': '请求成功'
+    }
+  }
 })
 
 module.exports = router
